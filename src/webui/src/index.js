cf6b0106082c0a4e5c2bbb531c32aec7216a9669
Viswanadh Kotha <11851996+viswanadh-kotha@users.noreply.github.com>
2020-06-19 10:02:07 -0700

    fix(webui): prevent occasional blank screen in Edge browser
    * Added polyfills
    * Added ggk-test.yaml
    * Removed dev dependencies
    * changes order or browsers list
    * Removing ggk-test.yaml file
    Co-authored-by: Rakesh <rakeshs@balluun.com>

--- src/webui/src/index.js
+++ src/webui/src/index.js
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved.
-
+import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
